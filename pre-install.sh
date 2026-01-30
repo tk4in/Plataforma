97de7ec312770dd28339d5f3481138480bf884af
tk4in <154823202+tk4in@users.noreply.github.com>
2026-01-30 17:49:50 -0300

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -d dominio -u usuario -p password"
+   echo -e "\t-d Dominio do site `exemplo.com`"
+   echo -e "\t-u Usuario `admin`"
+   echo -e "\t-p Senha do usuario `password`"
+   exit 1 # Exit script after printing help
+}
+
+while getopts d:u:p: opts; do
+   case ${opts} in
+      d) DOM_VAL=${OPTARG} ;;
+      u) USER_VAL=${OPTARG} ;;
+      p) PASS_VAL=${OPTARG} ;;
+   esac
+done
+
 # Atualizar a lista de pacotes e o sistema
+echo ""
 echo "Atualizando o sistema..."
-sudo su
 apt update -y && apt upgrade -y
 apt install -y gnupg apt-transport-https ca-certificates
 apt install -y nano curl wget git sed subversion alembic libjansson-dev autoconf automake libxml2-dev libncurses-dev libtool
 
 # Instalando o Firewall
+echo ""
 echo "Instalando o Firewall"
 apt install ufw
 ufw default deny incoming && ufw default allow outgoing
@@ -18,6 +37,7 @@ ufw allow http
 ufw enable
 
 # Alterando a porta do SSH
+echo ""
 echo "Alterando a porta do SSH"
 sed -i 's/#Port 22/Port 6922/g' /etc/ssh/sshd_config
 systemctl restart ssh
