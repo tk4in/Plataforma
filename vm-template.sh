67e2721c5b597c0706bfb3290a0730b6c3b72f53
tk4in <154823202+tk4in@users.noreply.github.com>
2026-05-08 00:21:49 -0300

    Add sudo to virt-install command and fix formatting

--- vm-template.sh
+++ vm-template.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-virt-install \
+sudo virt-install \
   --name $1-$2-$3-$4 \
   --vcpus $2 \
   --memory $3 \
-  --disk path=/var/lib/libvirt/images/$1-$2-$3-$4.qcow2,size=$4 ,format=qcow2 \
+  --disk path=/var/lib/libvirt/images/$1-$2-$3-$4.qcow2,size=$4,format=qcow2 \
   --location /var/lib/libvirt/images/$5 \
   --os-variant=$6 \
   --network network=default \
   --graphics none \
   --console pty,target_type=serial \
   --extra-args='console=ttyS0,115200n8'
+
+
+
+
+#virsh domifaddr $1-$2-$3-$4
+
+#virsh shutdown $1-$2-$3-$4
+#virt-sysprep -d $1-$2-$3-$4 --hostname server
+#virt-sparsify --compress \
+#  /var/lib/libvirt/images/$1-$2-$3-$4.qcow2 \
+#  /var/lib/libvirt/images/$1-$2-$3-$4.min.qcow2
+#mv /var/lib/libvirt/images/$1-$2-$3-$4.min.qcow2 \
+#  /var/lib/libvirt/images/$1-$2-$3-$4.qcow2
+#chmod 444 /var/lib/libvirt/images/$1-$2-$3-$4.qcow2
