8419cc424fb7f52d1513ffac802f5dfe296c2929
tk4in <154823202+tk4in@users.noreply.github.com>
2026-05-17 22:38:11 -0300

    Update dns-install.sh

--- DNS/dns-install.sh
+++ DNS/dns-install.sh
@@ -40,39 +40,4 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-echo -e "\n\e[32mInstalando o Firewall\e[0m"
-apk add ufw ip6tables
-ufw default deny incoming && ufw default allow outgoing
-ufw allow 53/tcp
-ufw allow 53/udp
-ufw allow ${SSH_PORT}/tcp
-yes | ufw enable
-
-echo -e "\n\e[32mAlterando o HostName\e[0m"
-echo -e "${DNS_VAL}" | tee /etc/hostname
-echo -e "127.0.0.1 localhost localhost.localdomain
-${DNS_IP} ://${DOM_VAL} ${DNS_VAL}
-::1 localhost ip6-localhost ip6-loopback" | tee /etc/hosts
-hostname -F /etc/hostname
-
-echo -e "\n\e[32mCriando o usuário\e[0m"
-adduser ${USER_VAL} <<EOF
-${PASS_VAL}
-${PASS_VAL}
-EOF
-
-echo -e "\n\e[32mInstalando o SSH na porta ${SSH_PORT}\e[0m"
-apk add openssh
-sed -i "s/#Port 22/Port ${SSH_PORT}/g" /etc/ssh/sshd_config
-rc-update add sshd
-/etc/init.d/sshd restart
-
-echo -e "\n\e[32mInstalando o Node/NPM\e[0m"
-apk add nodejs npm
-
-echo -e "\n\e[32mInstalando o Bind9\e[0m"
-apk add bind
-
-echo -e "\n\e[32mInstalando o End Point pra o DNS\e[0m"
-mkdir -p /home/${USER_VAL}/epdns
-wget -r -np -nH -O /var/home/${DOM_VAL}/epdns https://raw.githubusercontent.com/tk4in/Plataforma/refs/heads/master/DNS/epdns
+echo -e "${DNS_IP}"
