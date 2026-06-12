4810ce4a38fd0ecb19aeafb9cfb9a34f8bd40e91
tk4in <154823202+tk4in@users.noreply.github.com>
2026-06-12 10:15:00 -0300

    Upgrade Node.js version in dns-install.sh
    Updated Node.js version from 22.20.0 to 22.22.3 in dns-install.sh.

--- DNS/dns-install.sh
+++ DNS/dns-install.sh
@@ -72,8 +72,8 @@ rc-update add sshd default
 /etc/init.d/sshd restart
 
 echo -e "\n\e[32mInstalando o Node/NPM\e[0m"
-export NODE_VERSION="22.20.0"
-curl -O https://nodejs.org
+export NODE_VERSION="22.22.3"
+wget https://unofficial-builds.nodejs.org/download/release/v22.22.3/node-v$NODE_VERSION-linux-x64-musl.tar.gz
 mkdir -p /usr/local/lib/nodejs
 tar -xzf node-v$NODE_VERSION-linux-x64-musl.tar.gz -C /usr/local/lib/nodejs
 echo -e 'export PATH=/usr/local/lib/nodejs/node-v$NODE_VERSION-linux-x64-musl/bin:$PATH' >> /etc/bash/bashrc
