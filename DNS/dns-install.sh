5621c33c0f3f4e37922ca811122d148fd21f6b76
tk4in <154823202+tk4in@users.noreply.github.com>
2026-06-12 21:16:53 -0300

    Enhance network performance with sysctl settings
    Added various network and system performance tuning parameters to sysctl configuration files.

--- DNS/dns-install.sh
+++ DNS/dns-install.sh
@@ -134,6 +134,134 @@ _dmarc IN TXT "v=DMARC1; p=none; rua=mailto:dmarc@$DOM_VAL"` | tee /etc/bind/zon
 rc-update add named
 service named start
 
+# Liberar mais RAM para aceleração de rede
+(   
+    echo  "net.core.rmem_default=31457280";
+    echo  "net.core.wmem_default=31457280";
+    echo  "net.core.rmem_max=134217728";
+    echo  "net.core.wmem_max=134217728";
+    echo  "net.core.netdev_max_backlog=250000";
+    echo  "net.core.optmem_max=33554432";
+    echo  "net.core.default_qdisc=fq";
+    echo  "net.core.somaxconn=65535";
+) > /etc/sysctl.d/051-net-core.conf;
+
+# Aumentar capacidades de rede do protocolo TCP
+(
+    echo "net.ipv4.tcp_sack = 1";
+    echo "net.ipv4.tcp_timestamps = 1";
+    echo "net.ipv4.tcp_low_latency = 1";
+    echo "net.ipv4.tcp_max_syn_backlog = 8192";
+    echo "net.ipv4.tcp_rmem = 4096 87380 67108864";
+    echo "net.ipv4.tcp_wmem = 4096 65536 67108864";
+    echo "net.ipv4.tcp_mem = 6672016 6682016 7185248";
+    echo "net.ipv4.tcp_congestion_control=htcp";
+    echo "net.ipv4.tcp_mtu_probing=1";
+    echo "net.ipv4.tcp_moderate_rcvbuf =1";
+    echo "net.ipv4.tcp_no_metrics_save = 1";
+)  >  /etc/sysctl.d/052-net-tcp-ipv4.conf;
+
+# Ativar TCP-Fast-Open
+(
+    echo  "net.ipv4.tcp_fastopen=3";
+)  >  /etc/sysctl.d/053-tcp-fast-open.conf;
+
+# Ativar TCP-KeepAlive
+(
+    echo  "net.ipv4.tcp_keepalive_probes=9";
+    echo  "net.ipv4.tcp_keepalive_intvl=75";
+    echo  "net.ipv4.tcp_keepalive_time=7200";
+)  >  /etc/sysctl.d/054-tcp-keepalive.conf;
+
+# TTL padrão dos pacotes IPv4
+(
+    echo "net.ipv4.ip_default_ttl=128";
+) >  /etc/sysctl.d/062-default-ttl-ipv4.conf;
+
+# Ajustes de ARP e fragmentacao (maior capacidade) - IPv4
+(
+    echo "net.ipv4.neigh.default.gc_interval = 30";
+    echo "net.ipv4.neigh.default.gc_stale_time = 60";
+    echo "net.ipv4.neigh.default.gc_thresh1 = 4096";
+    echo "net.ipv4.neigh.default.gc_thresh2 = 8192";
+    echo "net.ipv4.neigh.default.gc_thresh3 = 12288";
+    echo;
+    echo "net.ipv4.ipfrag_high_thresh=4194304";
+    echo "net.ipv4.ipfrag_low_thresh=3145728";
+    echo "net.ipv4.ipfrag_max_dist=64";
+    echo "net.ipv4.ipfrag_secret_interval=0";
+    echo "net.ipv4.ipfrag_time=30";
+)  >  /etc/sysctl.d/063-neigh-ipv4.conf;
+
+# Ajustes de ARP e fragmentacao (maior capacidade) - IPv6
+(
+    echo "net.ipv6.neigh.default.gc_interval = 30";
+    echo "net.ipv6.neigh.default.gc_stale_time = 60";
+    echo "net.ipv6.neigh.default.gc_thresh1 = 4096";
+    echo "net.ipv6.neigh.default.gc_thresh2 = 8192";
+    echo "net.ipv6.neigh.default.gc_thresh3 = 12288";
+    echo;
+    echo "net.ipv6.ip6frag_high_thresh=4194304";
+    echo "net.ipv6.ip6frag_low_thresh=3145728";
+    echo "net.ipv6.ip6frag_secret_interval=0";
+    echo "net.ipv6.ip6frag_time=60";
+)  >  /etc/sysctl.d/064-neigh-ipv6.conf;
+
+# Ativar roteamento de pacotes IPv4
+(
+    echo  "net.ipv4.conf.default.forwarding=1"
+)  >  /etc/sysctl.d/065-default-foward-ipv4.conf;
+
+# Ativar roteamento de pacotes IPv6
+(
+    echo  "net.ipv6.conf.default.forwarding=1"
+) >  /etc/sysctl.d/066-default-foward-ipv6.conf;
+
+# Ativar roteamento em todas as interfaces de rede
+echo  "net.ipv4.conf.all.forwarding=1"   >  /etc/sysctl.d/067-all-foward-ipv4.conf
+echo  "net.ipv6.conf.all.forwarding=1"   >  /etc/sysctl.d/068-all-foward-ipv6.conf
+echo  "net.ipv4.ip_forward=1"            >  /etc/sysctl.d/069-ipv4-forward.conf
+
+# Aumentar capacidades de arquivos abertos
+(
+    echo "fs.file-max=2097152";
+    echo "fs.aio-max-nr=3263776";
+    echo "fs.mount-max=1048576";
+    echo "fs.mqueue.msg_max=128";
+    echo "fs.mqueue.msgsize_max=131072";
+    echo "fs.mqueue.queues_max=4096";
+    echo "fs.pipe-max-size=8388608";
+)  >  /etc/sysctl.d/072-fs-options.conf;
+
+# Nao usar SWAP enquanto houver memoria RAM livre
+echo  "vm.swappiness=0"            >  /etc/sysctl.d/073-swappiness.conf;
+
+# Usar mais RAM para priorizar metadados de sistema de arquivos
+echo  "vm.vfs_cache_pressure=50"   >  /etc/sysctl.d/074-vfs-cache-pressure.conf;
+
+# Flush escrita mais rápido
+(
+    echo "vm.dirty_ratio=5";
+    echo "vm.dirty_background_ratio=2";
+) > /etc/sysctl.d/075-dirty.conf;
+
+# Flexibilizar a alocacao de RAM para alem dos limites reais
+echo  "vm.overcommit_memory=1"     > /etc/sysctl.d/076-ram-overcommit.conf;
+
+# Reiniciar o kernel apos 10 segundos em caso de pane geral
+echo  "kernel.panic=10"            >  /etc/sysctl.d/081-kernel-panic.conf;
+
+# Comando (sysctl -p) requer tudo em um unico arquivo:
+(
+    echo;
+    cat /etc/sysctl.d/*.conf;
+    echo;
+) > /etc/sysctl.conf;
+
+# Aplicar imediatamente:
+sysctl -q --system  2>/dev/null;
+sysctl -q -p        2>/dev/null;
+
 echo -e "\n\e[32mAlterando o HostName/IP\e[0m"
 echo -e "${DNS_VAL}" | tee /etc/hostname
 echo -e "127.0.0.1 localhost localhost.localdomain
